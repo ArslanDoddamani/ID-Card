db19a4d608f153dcfa7bdfa6ad869fdbd38d456c
Arslan Doddamani <arslandoddamani11@gmail.com>
2024-11-13 19:26:43 +0530

    Add files via upload

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Card from "../3Dmodel/Card";
+
+const Home = () => {
+  return (
+    <>
+      <div className="w-full h-screen">
+        <Card />
+      </div>
+      <div className="w-full h-screen">
+      </div>
+      <div className="w-full h-screen">
+      </div>
+      <div className="w-full h-screen">
+      </div>
+    </>
+  );
+};
+
+export default Home;
